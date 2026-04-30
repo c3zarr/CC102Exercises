c14907f2675985efe3b7fd081c8027ed85ff893d
Layca <ty202502128@wmsu.edu.ph>
2026-04-30 14:00:33 +0800

    Create hitgano_A6.cpp

---
+++ hitgano_A6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;
+
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+void countPrimeNumbers() {
+    ifstream file("NUMS.TXT");
+    int num, count = 0;
+
+    while (file >> num) {
+        if (isPrime(num))
+            count++;
+    }
+
+    cout << "Number of prime numbers: " << count;
+}
+
+int main() {
+    countPrimeNumbers();
+    return 0;
+}
